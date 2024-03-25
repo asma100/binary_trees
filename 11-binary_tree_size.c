707c2139ee0cec3a2c99eb0d2050ed69a249f8bf
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 01:55:42 +0200

    tasksd1

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - the height
+ *
+ * @tree: Pointer to the tree node
+ *
+ *
+ * Return: size_t
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t left_size, right_size, max;
+if (tree == NULL)
+return (0);
+if (tree->left)
+left_size = 1 + binary_tree_size(tree->left);
+else
+left_size = 0;
+if (tree->right)
+right_size = 1 + binary_tree_size(tree->right);
+else
+right_size = 0;
+max = left_size + right_size;
+return (max);
+}
