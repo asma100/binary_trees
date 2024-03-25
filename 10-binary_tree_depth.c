5f3af4f2e2eaee500ad93b95b172363513924e30
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 01:47:18 +0200

    tasksd1

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,9 +10,10 @@
  *
  * Return: size_t
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_depth (const binary_tree_t *tree)
 {
 size_t depth = 0;
+
 if (tree && tree->parent)
 depth = 1 + binary_tree_depth(tree->parent);
 return (depth);
