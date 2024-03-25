7ff9cd22c360315f1fce42a26349cb29838dcd14
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 01:44:29 +0200

    tasksd1

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,27 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-size_t left_height, right_height, max;
-if (tree == NULL)
-return (0);
-if (tree->left)
-left_height = 1 + binary_tree_height(tree->left);
-else
-left_height = 0;
-if (tree->right)
-right_height = 1 + binary_tree_height(tree->right);
-else
-right_height = 0;
-if (left_height > right_height)
-max = left_height;
-else
-max = right_height;
-return (max);
-
-}
-
 
 /**
  * binary_tree_depth - the height
@@ -31,12 +10,10 @@ return (max);
  *
  * Return: size_t
  */
-size_t binary_tree_depth(const binary_tree_t *tree) {
-size_t tree_height;
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
 size_t depth;
-if (tree == NULL)
-return 0;
-tree_height = binary_tree_height(tree);
-depth = tree_height - binary_tree_height(tree->left) - binary_tree_height(tree->right) + 1;
+if (tree && tree->parent)
+depth = 1 + binary_tree_depth(tree->parent);
 return (depth);
 }
