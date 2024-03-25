a96175c6222273e47448552518f6ebf3fdd69344
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-25 15:16:08 +0200

    0-3

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right: insert_right
+ *
+ * @parent: Pointer to the parent node
+ * @value: int
+ *
+ * Return: binary_tree_t
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newr;
+    if(!parent) 
+        return (NULL);
+    newr= binary_tree_node(parent, value);
+    if(!newr)
+        return (NULL);
+
+    if(parent->right)
+	{
+		newr->right = parent->right;
+		parent->right->parent = newr;
+	}
+	parent->right = newr;
+
+	return (newr);
+}
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delete
+ *
+ * @tree: Pointer to the node to print
+
+ *
+ * Return: binary_tree_t
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+ binary_tree_t *i;
+for (i = tree; i->left == NULL; i = i->left)
+{
+    if(i->left == NULL)
+    delete_node(i);
+
+}
+for (i = tree; i->right == NULL; i = i->right)
+{
+    if(i->right == NULL)
+    delete_node(i);
+
+}
+delete_node(tree);
+
+}
+void delete_node(binary_tree_t *node)
+{
+if(node->left)
+free(node->left);
+node->left = NULL;
+if(node->right)
+free(node->right);
+node->right = NULL;
+free(node);
+}
