7d51acc4b8ec772d2a1ca7a90b9c63362797b493
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 00:10:51 +0200

    co

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,14 +15,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 if (tree == NULL)
 return (0);
 
-size_t left_height;
-size_t right_height;
-size_t max_height;
+size_t left_height = 0;
+size_t right_height = 0;
+size_t max_height = 0;
 left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 if (left_height > right_height)
 max_height = left_height;
 else
-max_height = right_height;
-return (max_height + 1);
+max_height = right_height + 1;
+return (max_height);
 }
