26c5da43f2e026c0bac2fb76c4bbc823304cbe52
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 09:56:23 +0200

    tasksd1

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,10 +13,10 @@ int binary_tree_is_child(const binary_tree_t *node)
 {
 if (node == NULL)
 return (0);
-if (node->left == NULL && node->right == NULL)
-return (0);
-else
+if (node->left != NULL || node->right != NULL)
 return (1);
+else
+return (0);
 }
 
 /**
