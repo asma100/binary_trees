f1e50d901f441bb36e4f8777287fa354a719d658
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 15:12:17 +0200

    tasksd2

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - sibling or not
+ * binary_tree_is_leaf - leaf or not
  *
  * @node: Pointer to the node
  *
- * Return: binary_tree_t
+ * Return: 1 or 0
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
@@ -19,3 +19,4 @@ return (node->parent->left);
 else
 return (NULL);
 }
+
