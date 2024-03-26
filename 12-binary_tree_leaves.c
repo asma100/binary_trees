b56b4fe9a5ff02fb74ac12bd65b9781e04023b99
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 09:30:36 +0200

    tasksd1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,17 +12,15 @@
  *
  * Return: size_t
  */
-
 size_t binary_tree_leaves(const binary_tree_t *tree)
  {
-  if (tree == NULL) {
-    return 0;
-  }
-
-  size_t leaves = 0;
-  leaves += binary_tree_is_leaf(tree);  // Call the function to check current node
-  leaves += binary_tree_leaves(tree->left);
-  leaves += binary_tree_leaves(tree->right);
-
-  return leaves;
+if (tree == NULL)
+{
+return 0;
+}
+size_t leaves = 0;
+leaves += binary_tree_is_leaf(tree);
+leaves += binary_tree_leaves(tree->left);
+leaves += binary_tree_leaves(tree->right);
+return leaves;
 }
