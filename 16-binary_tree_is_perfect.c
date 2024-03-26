78371b751f8593cdc2c096721caa07e1585784fa
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 14:21:14 +0200

    tasksd2

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -51,8 +51,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 if (tree)
 {
 size_t l_h = 0, r_h = 0;
-l_h = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-r_h = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+l_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+r_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 return ((l_h > r_h) ? l_h : r_h);
 }
 return (0);
@@ -87,7 +87,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 if(!tree)
 return(0);
-if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 1 )
+if (binary_tree_is_full(tree) == 1 && binary_tree_balance(tree) == 0 )
 return (1);
 else
 return (0);
