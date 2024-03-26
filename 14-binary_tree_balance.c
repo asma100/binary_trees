e7da308b16aee3e9e118adddd2726af117cff42a
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 13:54:54 +0200

    tasksd2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,11 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
-size_t binary_tree_height(const binary_tree_t *tree);
-    
-
-
-
+size_t binary_tree_height(const binary_tree_t *tree); 
 /**
  * binary_tree_balance - the height
  *
