4c095698d3434d4865e7c161c88af28557850a48
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 09:33:01 +0200

    tasksd1

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -28,12 +28,12 @@ return (0);
  * Return: size_t
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
- {
+{
+size_t leaves = 0;
 if (tree == NULL)
 {
 return 0;
 }
-size_t leaves = 0;
 leaves += binary_tree_is_leaf(tree);
 leaves += binary_tree_leaves(tree->left);
 leaves += binary_tree_leaves(tree->right);
