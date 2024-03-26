20cf4992d28942c436ed62c9231cf4bdc2a1ba53
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 10:49:53 +0200

    tasksd2

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - full or not
+ *
+ * @tree: Pointer to the tree
+ *
+ * Return: 1 or 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+if ((tree->left == NULL && tree->right == NULL) ||(tree->left != NULL && tree->right != NULL))
+return (1);
+else
+return (0);
+}
+
+
