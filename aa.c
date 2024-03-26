b7d7aaec554886764a10d58529132500fe37a369
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 02:02:59 +0200

    tasksd1

--- aa.c
+++ aa.c
@@ -7,6 +7,23 @@
  *
  * Return: Always 0 (Success)
  */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t left_size, right_size, max;
+if (tree == NULL)
+return (0);
+if (tree->left)
+left_size = 1 + binary_tree_size (tree->left);
+else
+left_size = 0;
+if (tree->right)
+right_size = 1 + binary_tree_size (tree->right);
+else
+right_size = 0;
+max = left_size + right_size;
+return (max);
+}
+
 int main(void)
 {
     binary_tree_t *root;
