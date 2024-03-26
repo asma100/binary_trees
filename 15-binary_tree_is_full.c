3b3b93e07581228f9d03029711d97c76aa42f49b
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 11:49:39 +0200

    tasksd2

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -21,11 +21,15 @@ return (0);
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL) {
-        return (0); 
-    }
+int full_1 , full_r , full_n , full ;
 
-    return (node_is_full(tree) && binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+if (tree == NULL)
+return (0);
+full_n = node_is_full(tree);
+full_1 = full_n * binary_tree_is_full(tree->left);
+full_r = full_n * binary_tree_is_full(tree->right);
+full = full_1 * full_r;
+return (full);
 }
 
 
