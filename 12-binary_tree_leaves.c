07f1db89ed8f9458a3cef60498baa159a23992b3
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 02:40:54 +0200

    tasksd1

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - the size
+ *
+ * @tree: Pointer to the tree node
+ *
+ *
+ * Return: size_t
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+size_t leaves = 0;
+size_t leaf;
+leaf = binary_tree_is_leaf;
+leaves = leaves + leaf;
+binary_tree_leaves(tree->left);
+binary_tree_leaves(tree->right);
+binary_tree_leaves(tree);
+}
