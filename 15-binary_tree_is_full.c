e2e6664265ec170d81f45dca67b7beef7f30c86b
asma100 <71214380+asma100@users.noreply.github.com>
2024-03-26 12:26:27 +0200

    tasksd2

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "binary_trees.h"
 /**
- * binary_tree_is_full - full or not
+ * node_is_full - full or not
  *
  * @tree: Pointer to the tree
  *
@@ -10,26 +10,28 @@
  */
 int node_is_full(const binary_tree_t *tree)
 {
- if (tree == NULL)
-        return 1;
-
-    if ((tree->left == NULL && tree->right == NULL) ||
-        (tree->left != NULL && tree->right != NULL))
-        return 1;
+if (tree == NULL)
+return (1);
+if ((tree->left == NULL && tree->right == NULL) ||
+(tree->left != NULL && tree->right != NULL))
+return (1);
 else
-return 0;
+return (0);
 }
-
+/**
+ * binary_tree_is_full - full or not
+ *
+ * @tree: Pointer to the tree
+ *
+ * Return: 1 or 0
+ */
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
-
- if (tree == NULL)
-        return 0; 
-
-    if (node_is_full(tree->left) == 1 && node_is_full(tree->right) == 1 && node_is_full(tree) == 1 )
-        return 1; 
-    else
-    return 0;
+if (tree == NULL)
+return (0); 
+if (node_is_full(tree->left) == 1 && node_is_full(tree->right) == 1 && node_is_full(tree) == 1)
+return (1); 
+else
+return (0);
 }
